9ae3ee63404e4d5a4a83052d8e733c495fcfb211
qzl <zl_qiu@163.com>
2021-11-08 16:27:52 +0800

    init repo

---
+++ src/main/java/com/qzl/bbs/Controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.qzl.bbs.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name", defaultValue = "name=null") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
